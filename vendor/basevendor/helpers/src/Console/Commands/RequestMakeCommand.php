76eb60b50bdff4b8ebe8c6793a139c5bf4a2bfd5
nguyendat <nguyen.van.dat@alliedtechbase.com>
2017-12-28 17:00:07 +0700

    Upload basevendor

---
+++ vendor/basevendor/helpers/src/Console/Commands/RequestMakeCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Basevendor\Helpers\Console\Commands;
+
+use Illuminate\Foundation\Console\RequestMakeCommand as BaseRequestMakeCommand;
+
+/**
+ * Class RequestMakeCommand.
+ *
+ * @package App\Console\Commands
+ */
+class RequestMakeCommand extends BaseRequestMakeCommand
+{
+    use PackableGeneratorCommand;
+}
